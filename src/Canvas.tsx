ae7ffaffb53ecbfa8d6a3586489b67c1f848b551
davidahirshberg <david.a.hirshberg@gmail.com>
2026-02-05 03:15:47 -0500

    Add DOM mutation logging

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -12,6 +12,20 @@ export function Canvas({ roomId: _roomId, onLoadPdf: _onLoadPdf }: CanvasProps) 
 
   useEffect(() => {
     console.log('[Canvas] Mounted')
+
+    // Monitor for DOM changes
+    const container = document.querySelector('.tl-container')
+    if (container) {
+      const observer = new MutationObserver((mutations) => {
+        console.log('[Canvas] DOM mutation:', mutations.length, 'changes')
+        if (container.children.length === 0) {
+          console.log('[Canvas] CONTAINER EMPTIED!')
+        }
+      })
+      observer.observe(container, { childList: true, subtree: true })
+      return () => observer.disconnect()
+    }
+
     return () => console.log('[Canvas] UNMOUNTING!')
   }, [])
 
